d3239da8d08124ff071f6d7707d0cebd1c80a76f
EvgenGal1 <EvgenGal1@users.noreply.github.com>
2020-12-30 18:10:47 +0300

    1:03:55 адапт шапка, бургер - всё. перед др. блоками

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,37 +1,10 @@
 // script.js =====================================================================================
-var isMobile = {
-  Android: function () {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBarry: function () {
-    return navigator.userAgent.match(/BlackBarry/i);
-  },
-};
-if (isMobile.any()) {
-}
 
-if (location.hash) {
-  var hsh = location.hash.replace("#", "");
-  if ($(".popup-" + hsh).length > 0) {
-    popupOpen(hsh);
-  } else if ($("div." + hsh).length > 0) {
-    $("body,html").animate(
-      { scrollTop: $("div." + hsh).offset().top },
-      500,
-      function () {}
-    );
-  }
-}
-$(".wrapper").addClass("loaded");
-
-var act = "click";
-if (isMobile.iOS()) {
-  var act = "touchstart";
-}
-
+// burger. нажатие на кнопку, приписка класса, скролл
+// при нажатии на бургер классу header-burger добавиться класс active
 $(".header-menu__icon").click(function (event) {
   $(this).toggleClass("active");
-  $(".header-menu").toggleClass("active");
+  $(".header-burger").toggleClass("active");
   if ($(this).hasClass("active")) {
     $("body").data("scroll", $(window).scrollTop());
   }
@@ -41,16 +14,47 @@ $(".header-menu__icon").click(function (event) {
   }
 });
 
-// ZOOM
-if ($(".gallery").length > 0) {
-  baguetteBox.run(".gallery", {
-    //Custom option
-  });
-}
+// var isMobile = {
+//   Android: function () {
+//     return navigator.userAgent.match(/Android/i);
+//   },
+//   BlackBarry: function () {
+//     return navigator.userAgent.match(/BlackBarry/i);
+//   },
+// };
+// if (isMobile.any()) {
+// }
 
-// POPUP
-$(".pl").click(function (event) {
-  var pl = $(this).attr("href").replace("#", "");
-  var v = $(this).data("vid");
-  popupOpen(pl, v);
-});
+// if (location.hash) {
+//   var hsh = location.hash.replace("#", "");
+//   if ($(".popup-" + hsh).length > 0) {
+//     popupOpen(hsh);
+//   } else if ($("div." + hsh).length > 0) {
+//     $("body,html").animate(
+//       { scrollTop: $("div." + hsh).offset().top },
+//       500,
+//       function () {}
+//     );
+//   }
+// }
+// $(".wrapper").addClass("loaded");
+
+// var act = "click";
+// if (isMobile.iOS()) {
+//   var act = "touchstart";
+// }
+
+// // ZOOM
+// if ($(".gallery").length > 0) {
+//   baguetteBox.run(".gallery", {
+//     //Custom option
+//   });
+// }
+
+// // POPUP
+// $(".pl").click(function (event) {
+//   var pl = $(this).attr("href").replace("#", "");
+//   var v = $(this).data("vid");
+//   popupOpen(pl, v);
+//   return false;
+// });
--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@ function adaptive_function(width) {
   var headerLang = $(".header-top-lang");
   // класс меню
   var headerMenu = $(".header-bottom-menu");
-  // е\и ширина меньше 767
-  if (width < 767) {
+  // е\и ширина меньше 751
+  if (width < 751) {
     // е\и в языках нет класса done
     if (!headerLang.hasClass("done")) {
       // то блоку header-top-lang добавит класс "done" и добавить его в блок header-burger
@@ -42,8 +42,8 @@ function adaptive_function(width) {
       headerLang.removeClass("done").prependTo($(".header-top"));
     }
   }
-  // е\и ширина меньше 767
-  if (width < 767) {
+  // е\и ширина меньше 751
+  if (width < 751) {
     // е\и у меню нет класса done
     if (!headerMenu.hasClass("done")) {
       // то блоку header-bottom-menu добавит класс "done" и добавить его в блок header-burger
@@ -87,39 +87,12 @@ function adaptive_function(width) {
 // forms.js =====================================================================================
 
 // script.js =====================================================================================
-var isMobile = {
-  Android: function () {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBarry: function () {
-    return navigator.userAgent.match(/BlackBarry/i);
-  },
-};
-if (isMobile.any()) {
-}
 
-if (location.hash) {
-  var hsh = location.hash.replace("#", "");
-  if ($(".popup-" + hsh).length > 0) {
-    popupOpen(hsh);
-  } else if ($("div." + hsh).length > 0) {
-    $("body,html").animate(
-      { scrollTop: $("div." + hsh).offset().top },
-      500,
-      function () {}
-    );
-  }
-}
-$(".wrapper").addClass("loaded");
-
-var act = "click";
-if (isMobile.iOS()) {
-  var act = "touchstart";
-}
-
+// burger. нажатие на кнопку, приписка класса, скролл
+// при нажатии на бургер классу header-burger добавиться класс active
 $(".header-menu__icon").click(function (event) {
   $(this).toggleClass("active");
-  $(".header-menu").toggleClass("active");
+  $(".header-burger").toggleClass("active");
   if ($(this).hasClass("active")) {
     $("body").data("scroll", $(window).scrollTop());
   }
@@ -129,19 +102,50 @@ $(".header-menu__icon").click(function (event) {
   }
 });
 
-// ZOOM
-if ($(".gallery").length > 0) {
-  baguetteBox.run(".gallery", {
-    //Custom option
-  });
-}
+// var isMobile = {
+//   Android: function () {
+//     return navigator.userAgent.match(/Android/i);
+//   },
+//   BlackBarry: function () {
+//     return navigator.userAgent.match(/BlackBarry/i);
+//   },
+// };
+// if (isMobile.any()) {
+// }
 
-// POPUP
-$(".pl").click(function (event) {
-  var pl = $(this).attr("href").replace("#", "");
-  var v = $(this).data("vid");
-  popupOpen(pl, v);
-});
+// if (location.hash) {
+//   var hsh = location.hash.replace("#", "");
+//   if ($(".popup-" + hsh).length > 0) {
+//     popupOpen(hsh);
+//   } else if ($("div." + hsh).length > 0) {
+//     $("body,html").animate(
+//       { scrollTop: $("div." + hsh).offset().top },
+//       500,
+//       function () {}
+//     );
+//   }
+// }
+// $(".wrapper").addClass("loaded");
+
+// var act = "click";
+// if (isMobile.iOS()) {
+//   var act = "touchstart";
+// }
+
+// // ZOOM
+// if ($(".gallery").length > 0) {
+//   baguetteBox.run(".gallery", {
+//     //Custom option
+//   });
+// }
+
+// // POPUP
+// $(".pl").click(function (event) {
+//   var pl = $(this).attr("href").replace("#", "");
+//   var v = $(this).data("vid");
+//   popupOpen(pl, v);
+//   return false;
+// });
 
 // js-end.js =====================================================================================
 
